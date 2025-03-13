6d0f69088c6dce6735c1e24eeed77267c58059bf
Tobi <testta155afi1ra@>
2025-03-13 23:16:14 +0100

    Created kind of working model to detect circular motion

--- src/model/normalization_params.h
+++ src/model/normalization_params.h
@@ -2,9 +2,9 @@
 #define NORMALIZATION_PARAMS_H
 
 // Normalization parameters for accelerometer and gyroscope data, automatically generated when training model
-#define ACC_MEAN 0.045339
-#define ACC_STD 0.913762
-#define GYRO_MEAN -5.904769
-#define GYRO_STD 157.831150
+#define ACC_MEAN 0.045479
+#define ACC_STD 0.757135
+#define GYRO_MEAN -1.434857
+#define GYRO_STD 131.706959
 
 #endif // NORMALIZATION_PARAMS_H
